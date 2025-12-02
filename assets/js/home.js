0487a78cdaf2dd9c7e97b09980fb86d4ab6707a9
suegin <166367284+suegin@users.noreply.github.com>
2025-12-02 17:00:09 +0900

    up

--- assets/js/home.js
+++ assets/js/home.js
@@ -2,9 +2,9 @@ window.addEventListener("load", () => {
   const splash = document.getElementById("splash");
   const content = document.getElementById("content");
 
-  // アニメーション時間と合わせる（今回は2秒）
+  // アニメーション時間と合わせる
   setTimeout(() => {
     splash.style.display = "none";
     content.style.display = "block";
-  }, 2000);
+  }, 500);
 });
